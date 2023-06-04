9a850ad5599a4387ef2f57be05c69fd46d01d200
Victor Emanuel <78376383+vicnuel@users.noreply.github.com>
2023-06-04 19:06:32 -0300

    add result's styles

--- src/components/Form/ResultImc/index.js
+++ src/components/Form/ResultImc/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 import { StyleSheet, Text, View } from "react-native"
 
+import styles from "./styles"
+
 export default function ResultImc({ messageImc, resultImc }) {
     return (
-        <View>
-            <Text>{messageImc}</Text>
-            <Text>{resultImc}</Text>
+        <View style={styles.resultImc}>
+            <Text style={styles.information}>{messageImc}</Text>
+            <Text style={styles.numberImc}>{resultImc}</Text>
         </View>
     )
 }
