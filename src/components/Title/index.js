1bbf19f6694b91db607edf671366e04e4f28ad19
Victor Emanuel <78376383+vicnuel@users.noreply.github.com>
2023-06-04 11:42:41 -0300

    add title

---
+++ src/components/Title/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { StyleSheet, Text, View } from "react-native"
+
+export default function Title() {
+    return (
+        <View>
+            <Text>Vicnuel Health</Text>
+        </View>
+    )
+}
